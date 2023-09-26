b050cdbbac3dc5699a0678c6ad2b685dc1a73e41
vpapudesibabu <vpapudesibabu@hawk.iit.edu>
2023-09-26 16:19:18 -0500

    Update files-generator.py

--- files-generator.py
+++ files-generator.py
@@ -8,7 +8,7 @@ print(os.getcwd())
 
 peer = sys.argv[1]
 
-filesDirectory = os.getcwd()+"/datasets/"
+filesDirectory = os.getcwd()+"/bin/datasets/"
 
 if not os.path.exists(filesDirectory):
     os.makedirs(filesDirectory)
