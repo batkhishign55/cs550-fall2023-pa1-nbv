e253f204389e53a592dbe9b4a78054df9a938516
vpapudesibabu <vpapudesibabu@hawk.iit.edu>
2023-09-26 23:23:07 -0500

    files_gen

--- files-generator.py
+++ files-generator.py
@@ -13,17 +13,17 @@ filesDirectory = os.getcwd()+"/bin/"+peer+"/"
 if not os.path.exists(filesDirectory):
     os.makedirs(filesDirectory)
 
-def generate_files(size_in_bytes, files_count, file_type):
+def generate_files(bytetype, size_in_bytes, files_count, file_type):
     for i in range(0, files_count):
         extension = ".txt" if file_type=="text" else ".bin"
-        filename = "1KB_"+"0"*(6-len(str(i)))+str(i)+"_"+peer+extension
+        filename = bytetype+"0"*(6-len(str(i)))+str(i)+"_"+peer+extension
         file_path = os.path.join(filesDirectory, filename)
 
         with open(file_path, 'wb') as file:
             content = bytes(os.urandom(size_in_bytes))
             file.write(content)
 
-generate_files(1024, 500000, "text")
-generate_files(1024*1024, 5000, "text")
-generate_files(1024*1024*1024, 5, "binary")
+generate_files("1MB_", 1024, 500000, "text")
+generate_files("1MB_", 1024*1024, 5000, "text")
+generate_files("1GB_", 1024*1024*1024, 5, "binary")
 
