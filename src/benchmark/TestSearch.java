04a8c133cc08063d3cd9c6590067048d701b2112
batkhishig <batkhishign55@gmail.com>
2023-10-20 12:17:24 -0500

    test code improved

--- src/benchmark/TestSearch.java
+++ src/benchmark/TestSearch.java
@@ -2,91 +2,119 @@ package src.benchmark;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class TestSearch {
 
-    private static final String[] peers = { "peer1", "peer2" };
-    private static final String[] sizes = { "1KB", "1MB", "1GB" };
-    private static final int[] sizeLimits = { 10000, 1000, 10 };
+    private String[] peers = new String[15];
+    private static final String[] sizes = { "10KB", "100MB" };
+    private static final int[] sizeLimits = { 100000, 10 };
     private static final Random random = new Random();
+    List<Long> timeList = new ArrayList<>();
+    Properties prop;
+    private int testType;
 
-    public static String getRandomFilename() {
-        String size = sizes[random.nextInt(sizes.length)];
-        int limit = sizeLimits[random.nextInt(sizeLimits.length)];
+    private String getRandomFilename() {
+        String size = sizes[this.testType];
+        int limit = sizeLimits[this.testType];
         String value = String.format("%06d", random.nextInt(limit));
-        String extension = (size.equals("1KB") || size.equals("1MB")) ? ".txt" : ".bin";
+        String extension = ".txt";
         String peer = peers[random.nextInt(peers.length)];
         return size + "_" + value + "_" + peer + extension;
     }
 
-    public TestSearch() {
+    public TestSearch(String testType) {
+        this.testType = Integer.parseInt(testType);
+
+        prop = new Properties();
+        try (FileInputStream fis = new FileInputStream("app.config")) {
+            prop.load(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int idx = 0;
+        for (int i = 0; i < 16; i++) {
+            String peerId = String.format("peer%d", i + 1);
+            if (!prop.getProperty("id").equals(peerId)) {
+                this.peers[idx] = peerId;
+                idx += 1;
+                System.out.println(peerId);
+            }
+        }
+
+        // record start time
+        long startTime = System.nanoTime();
+
+        // establish a connection
+        for (int i = 0; i < 10000; i++) {
+            this.sendRequest();
+        }
+        // record end time
+        long endTime = System.nanoTime();
+
+        // calculating the time in milliseconds
+        long elapsedTimeMillis = (endTime - startTime) / 1000000;
+
+        System.out.println("Total Time Taken: " + elapsedTimeMillis + "ms");
+        System.out.println("Time Taken Per Request: " + this.timeList);
+
+    }
+
+    private void sendRequest() {
+        // record start time
+        long startTime = System.nanoTime();
+
+        Socket socket = null;
+        DataOutputStream out = null;
+        DataInputStream in = null;
+
+        try {
+
+            socket = new Socket(this.prop.getProperty("cis_address"), 8080);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            // send request type
+            out.writeUTF("search");
+
+            // send fileName
+            out.writeUTF(getRandomFilename());
+            String msg = in.readUTF();
+            // reads peers from cis until "end" is sent
+            while (!msg.equals("end")) {
+                System.out.println(msg);
+                msg = in.readUTF();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // calculating the time in milliseconds
+        long elapsedTimeMillis = (System.nanoTime() - startTime) / 1000000;
+        this.timeList.add(elapsedTimeMillis);
+
     }
 
     public static void main(String args[]) {
-        // record start time    
-        long startTime = System.nanoTime();
-        List<Long> TimeList = new ArrayList<>();
-        
-        // establish a connection
-
-        for (int i = 0; i < 10000; i++) {
-            // record start time    
-            long startTime1 = System.nanoTime();
-
-            Socket socket = null;
-            DataOutputStream out = null;
-            DataInputStream in = null;
-
-            try {
-
-                socket = new Socket("127.0.0.1", 8080);
-                in = new DataInputStream(socket.getInputStream());
-                out = new DataOutputStream(socket.getOutputStream());
-
-                // send request type
-                out.writeUTF("search");
-
-                // send fileName
-                out.writeUTF(getRandomFilename());
-                String msg = in.readUTF();
-                // reads peers from cis until "end" is sent
-                while (!msg.equals("end")) {
-                    System.out.println(msg);
-                    msg = in.readUTF();
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    in.close();
-                    out.close();
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            // record end time
-            long endTime = System.nanoTime();
-
-            // calculating the time in milliseconds
-            long elapsedTimeMillis = (endTime - startTime1) / 1000000; 
-            TimeList.add(elapsedTimeMillis);
-        }
-        // record end time
-         long endTime = System.nanoTime();
-
-         // calculating the time in milliseconds
-         long elapsedTimeMillis = (endTime - startTime) / 1000000; 
-
-         System.out.println("Total Time Taken: " + elapsedTimeMillis + "ms");
-         System.out.println("Time Taken Per Request: " + TimeList);
-         
-   
+        new TestSearch(args[0]);
     }
 }
--- src/peer/PeerClient.java
+++ src/peer/PeerClient.java
@@ -58,7 +58,7 @@ public class PeerClient extends Thread {
                         break;
                 }
             } catch (IOException e) {
-                System.out.println("[Client]: Encountered an error while registering!");
+                System.out.println("[Client]: Encountered an error while handling your request!");
                 e.printStackTrace();
             } finally {
                 try {
@@ -113,9 +113,6 @@ public class PeerClient extends Thread {
 
     private void search() throws UnknownHostException, IOException {
 
-        // record start time
-        long startTime = System.nanoTime();
-
         System.out.print("[Client]: File name:");
         String fileName = input.nextLine();
         if (fileName == null || fileName == "") {
@@ -145,21 +142,9 @@ public class PeerClient extends Thread {
             this.peers.add(msg);
             msg = in.readUTF();
         }
-
-        // record end time
-        long endTime = System.nanoTime();
-
-        // calculating the time in milliseconds
-        long elapsedTimeMillis = (endTime - startTime) / 1000000;
-
-        System.out.println("Time Taken: " + elapsedTimeMillis + "ms");
-
     }
 
     private void obtain() throws UnknownHostException, IOException {
-
-        // record start time
-        long startTime = System.nanoTime();
 
         if (this.fileToObtain == "") {
             System.out.println("[Client]: Please search a file first!");
@@ -211,12 +196,5 @@ public class PeerClient extends Thread {
                 fos.write(buffer, 0, bytesRead);
             }
         }
-        // record end time
-        long endTime = System.nanoTime();
-
-        // calculating the time in milliseconds
-        long elapsedTimeMillis = (endTime - startTime) / 1000000;
-
-        System.out.println("Time Taken: " + elapsedTimeMillis + "ms");
     }
 }
